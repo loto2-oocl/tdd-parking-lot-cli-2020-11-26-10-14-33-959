9351474e4c57ae8cb96d4c80cfd05500f97e0ad1
Tom Lo <loto2@oocl.com>
2020-11-27 15:18:06 +0800

    [parking-lot][Tom] test: implement test for multiple cars should be parked when park car given multiple cars and enough capacity

--- src/test/java/com/oocl/cultivation/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotTest.java
@@ -44,4 +44,22 @@ class ParkingLotTest {
         // THEN
         assertNull(ticket);
     }
+
+    @Test
+    void should_multiple_cars_parked_with_ticket_when_park_car_given_multiple_cars_and_enough_capacity() {
+        // GIVEN
+        Car car1 = new Car();
+        Car car2 = new Car();
+        ParkingLot parkingLot = new ParkingLot(2);
+
+        // WHEN
+        Ticket ticket1 = parkingLot.park(car1);
+        Ticket ticket2 = parkingLot.park(car2);
+
+        // THEN
+        assertTrue(parkingLot.getCarTicketHashMap().containsKey(car1));
+        assertTrue(parkingLot.getCarTicketHashMap().containsKey(car2));
+        assertEquals(ticket1, parkingLot.getCarTicketHashMap().get(car1));
+        assertEquals(ticket2, parkingLot.getCarTicketHashMap().get(car2));
+    }
 }
