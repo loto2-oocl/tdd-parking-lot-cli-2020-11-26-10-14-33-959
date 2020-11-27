ef06992f7a3e68497fec59dfbd2864d51970fcc8
Tom Lo <loto2@oocl.com>
2020-11-27 15:37:27 +0800

    [parking-lot][Tom] refactor: remove testcase for ParkingLot in ParkingBoyTest

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -24,20 +24,5 @@ class ParkingBoyTest {
         // Then
         verify(parkingLot, times(1)).park(car);
     }
-
-    @Test
-    void should_return_ticket_and_parked_car_when_park_car_given_a_car_and_parking_lot_with_enough_capacity() {
-        // GIVEN
-        Car car = new Car();
-        ParkingLot parkingLot = new ParkingLot(1);
-        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
-
-        // WHEN
-        Ticket ticket = parkingBoy.park(car);
-
-        // THEN
-        assertNotNull(ticket);
-        assertEquals(ticket, parkingLot.getCarTicketHashMap().get(car));
-    }
 }
 
