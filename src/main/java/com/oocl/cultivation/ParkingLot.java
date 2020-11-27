cef8cdc5c0d4d76606042115a41703c1a63ef77b
Tom Lo <loto2@oocl.com>
2020-11-27 15:04:54 +0800

    [parking-lot][Tom] test: implement test for parking lot should park one car given a car and enough capacity

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,6 +1,20 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+
 public class ParkingLot {
+    private final Integer capacity;
+    private final HashMap<Car, Ticket> carTicketHashMap;
+
+    public ParkingLot(Integer capacity) {
+        this.capacity = capacity;
+        this.carTicketHashMap = new HashMap<Car, Ticket>();
+    }
+
+    public HashMap<Car, Ticket> getCarTicketHashMap() {
+        return carTicketHashMap;
+    }
+
     public Ticket park(Car car) {
         return new Ticket();
     }
--- src/test/java/com/oocl/cultivation/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotTest.java
@@ -9,7 +9,7 @@ class ParkingLotTest {
     void should_return_one_parking_ticket_when_park_the_car_given_a_car_and_enough_capacity() {
         // GIVEN
         Car car = new Car();
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(1);
 
         // WHEN
         Ticket ticket = parkingLot.park(car);
@@ -18,4 +18,16 @@ class ParkingLotTest {
         assertNotNull(ticket);
     }
 
+    @Test
+    void should_parked_one_car_when_park_the_car_given_a_car_and_enough_capacity() {
+        // GIVEN
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot(10);
+
+        // WHEN
+        Ticket ticket = parkingLot.park(car);
+
+        // THEN
+        assertNotNull(parkingLot.getCarTicketHashMap().get(car));
+    }
 }
