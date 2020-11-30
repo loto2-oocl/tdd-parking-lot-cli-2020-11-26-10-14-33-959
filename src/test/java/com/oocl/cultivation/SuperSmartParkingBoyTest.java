17f903457752c04022eed6cc0dfa3acd9170e661
Tom Lo <loto2@oocl.com>
2020-11-30 10:55:11 +0800

    [parking-lot][Tom] refactor: mock parkingLot.getAvailablePositionRate for SuperSmartParkingBoyTest

--- src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -2,33 +2,34 @@ package com.oocl.cultivation;
 
 import com.oocl.cultivation.exception.NotEnoughPositionException;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class SuperSmartParkingBoyTest {
     @Test
     void should_park_at_first_car_park_when_park_given_a_car_and_super_smart_parking_boy_with_two_parking_lots_when_first_has_larger_available_position_rate()
         throws NotEnoughPositionException {
         // GIVEN
-        ParkingLot parkingLot1 = new ParkingLot(1);
-        ParkingLot parkingLot2 = new ParkingLot(3);
-        parkingLot2.park(new Car());
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(parkingLot1);
-        parkingLots.add(parkingLot2);
+        ParkingLot parkingLot1 = Mockito.mock(ParkingLot.class);
+        ParkingLot parkingLot2 = Mockito.mock(ParkingLot.class);
+        List<ParkingLot> parkingLots = new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2));
+        when(parkingLot1.isParkingLotAvailable()).thenReturn(true);
+        when(parkingLot2.isParkingLotAvailable()).thenReturn(true);
+        when(parkingLot1.getAvailablePositionRate()).thenReturn(0.5);
+        when(parkingLot2.getAvailablePositionRate()).thenReturn(0.1);
 
         SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
         Car car = new Car();
 
         // WHEN
-        Ticket ticket = superSmartParkingBoy.park(car);
+        superSmartParkingBoy.park(car);
 
         // THEN
-        assertNotNull(ticket);
-        assertTrue(parkingLot1.isInParkingLot(ticket));
-        assertEquals(car, parkingLot1.getTicketCarHashMap().get(ticket));
+        verify(parkingLot1, times(1)).park(car);
     }
 }
