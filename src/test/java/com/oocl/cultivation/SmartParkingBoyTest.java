a6c2445f0fa92de581a9b692a9dc95f07507576b
Tom Lo <loto2@oocl.com>
2020-11-30 10:54:45 +0800

    [parking-lot][Tom] refactor: mock parkingLot.getRemainingCapcitiy for SmartParkingBoyTest

--- src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -2,32 +2,34 @@ package com.oocl.cultivation;
 
 import com.oocl.cultivation.exception.NotEnoughPositionException;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class SmartParkingBoyTest {
     @Test
     void should_park_at_second_car_park_when_park_given_a_car_and_smart_parking_boy_with_two_parking_lots_when_second_has_more_capacity()
         throws NotEnoughPositionException {
         // GIVEN
-        ParkingLot parkingLot1 = new ParkingLot(5);
-        ParkingLot parkingLot2 = new ParkingLot(10);
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(parkingLot1);
-        parkingLots.add(parkingLot2);
+        ParkingLot parkingLot1 = Mockito.mock(ParkingLot.class);
+        ParkingLot parkingLot2 = Mockito.mock(ParkingLot.class);
+        List<ParkingLot> parkingLots = new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2));
+        when(parkingLot1.isParkingLotAvailable()).thenReturn(true);
+        when(parkingLot2.isParkingLotAvailable()).thenReturn(true);
+        when(parkingLot1.getRemainingCapacity()).thenReturn(10);
+        when(parkingLot2.getRemainingCapacity()).thenReturn(100);
 
         SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
         Car car = new Car();
 
         // WHEN
-        Ticket ticket = smartParkingBoy.park(car);
+        smartParkingBoy.park(car);
 
         // THEN
-        assertNotNull(ticket);
-        assertTrue(parkingLot2.isInParkingLot(ticket));
-        assertEquals(car, parkingLot2.getTicketCarHashMap().get(ticket));
+        verify(parkingLot2, times(1)).park(car);
     }
 }
