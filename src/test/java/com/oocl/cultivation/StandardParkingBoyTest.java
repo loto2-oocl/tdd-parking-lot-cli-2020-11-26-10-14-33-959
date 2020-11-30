f9153898f53209d92e3b6147646329b933f89db7
Tom Lo <loto2@oocl.com>
2020-11-30 10:41:49 +0800

    [parking-lot][Tom] refactor: mock parking lot full/available logic for StandardParkingBoyTest

--- src/test/java/com/oocl/cultivation/StandardParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/StandardParkingBoyTest.java
@@ -6,9 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 class StandardParkingBoyTest {
@@ -53,64 +54,59 @@ class StandardParkingBoyTest {
     void should_car_parked_at_first_parking_lot_when_park_car_given_a_car_and_parking_boy_with_two_parking_lot_at_enough_capacity()
         throws NotEnoughPositionException {
         // GIVEN
-        ParkingLot parkingLot1 = new ParkingLot(10);
-        ParkingLot parkingLot2 = new ParkingLot(10);
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(parkingLot1);
-        parkingLots.add(parkingLot2);
+        ParkingLot parkingLot1 = Mockito.mock(ParkingLot.class);
+        ParkingLot parkingLot2 = Mockito.mock(ParkingLot.class);
+        List<ParkingLot> parkingLots = new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2));
+        when(parkingLot1.isParkingLotAvailable()).thenReturn(true);
+        when(parkingLot2.isParkingLotAvailable()).thenReturn(true);
 
         StandardParkingBoy standardParkingBoy = new StandardParkingBoy(parkingLots);
         Car car = new Car();
 
         // WHEN
-        Ticket ticket = standardParkingBoy.park(car);
+        standardParkingBoy.park(car);
 
         // THEN
-        assertNotNull(ticket);
-        assertTrue(parkingLot1.getTicketCarHashMap().containsKey(ticket));
-        assertEquals(car, parkingLot1.getTicketCarHashMap().get(ticket));
+        verify(parkingLot1, times(1)).park(car);
     }
 
     @Test
     void should_car_parked_at_second_parking_lot_when_park_car_given_a_car_and_parking_boy_with_two_parking_lot_with_only_second_has_enough_capacity()
         throws NotEnoughPositionException {
         // GIVEN
-        ParkingLot parkingLot1 = new ParkingLot(0);
-        ParkingLot parkingLot2 = new ParkingLot(10);
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(parkingLot1);
-        parkingLots.add(parkingLot2);
+        ParkingLot parkingLot1 = Mockito.mock(ParkingLot.class);
+        ParkingLot parkingLot2 = Mockito.mock(ParkingLot.class);
+        List<ParkingLot> parkingLots = new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2));
+        when(parkingLot1.isParkingLotAvailable()).thenReturn(false);
+        when(parkingLot2.isParkingLotAvailable()).thenReturn(true);
 
         StandardParkingBoy standardParkingBoy = new StandardParkingBoy(parkingLots);
         Car car = new Car();
 
         // WHEN
-        Ticket ticket = standardParkingBoy.park(car);
+        standardParkingBoy.park(car);
 
         // THEN
-        assertNotNull(ticket);
-        assertTrue(parkingLot2.getTicketCarHashMap().containsKey(ticket));
-        assertEquals(car, parkingLot2.getTicketCarHashMap().get(ticket));
+        verify(parkingLot2, times(1)).park(car);
     }
 
     @Test
     void should_throw_not_enough_position_exception_when_park_car_give_a_car_and_parking_boy_with_two_parking_lot_with_not_enough_capacity() {
         // GIVEN
-        ParkingLot parkingLot1 = new ParkingLot(0);
-        ParkingLot parkingLot2 = new ParkingLot(0);
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(parkingLot1);
-        parkingLots.add(parkingLot2);
+        ParkingLot parkingLot1 = Mockito.mock(ParkingLot.class);
+        ParkingLot parkingLot2 = Mockito.mock(ParkingLot.class);
+        List<ParkingLot> parkingLots = new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2));
+        when(parkingLot1.isParkingLotAvailable()).thenReturn(false);
+        when(parkingLot2.isParkingLotAvailable()).thenReturn(false);
 
         StandardParkingBoy standardParkingBoy = new StandardParkingBoy(parkingLots);
-        Car car = new Car();
 
         // THEN
         assertThrows(
             NotEnoughPositionException.class,
             () -> {
                 // WHEN
-                standardParkingBoy.park(car);
+                standardParkingBoy.park(new Car());
             }
             , "Not enough position."
         );
@@ -120,35 +116,35 @@ class StandardParkingBoyTest {
     void should_fetch_car_from_second_car_park_when_fetch_car_given_a_ticket_and_parking_boy_with_two_parking_lot_and_one_parked_the_target_car()
         throws NotEnoughPositionException, UnrecognizedParkingTicketException {
         // GIVEN
+        Ticket ticket = new Ticket();
         Car car = new Car();
-        ParkingLot parkingLot1 = new ParkingLot(1);
-        ParkingLot parkingLot2 = new ParkingLot(1);
-        Ticket ticket = parkingLot2.park(car);
+        ParkingLot parkingLot1 = Mockito.mock(ParkingLot.class);
+        ParkingLot parkingLot2 = Mockito.mock(ParkingLot.class);
+        List<ParkingLot> parkingLots = new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2));
+        when(parkingLot1.isInParkingLot(ticket)).thenReturn(false);
+        when(parkingLot2.isInParkingLot(ticket)).thenReturn(true);
 
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(parkingLot1);
-        parkingLots.add(parkingLot2);
         StandardParkingBoy standardParkingBoy = new StandardParkingBoy(parkingLots);
 
         // WHEN
-        Car actual = standardParkingBoy.fetchCar(ticket);
+        standardParkingBoy.fetchCar(ticket);
 
         // THEN
-        assertEquals(car, actual);
+        verify(parkingLot2, times(1)).fetchCar(ticket);
     }
 
     @Test
     void should_throw_unrecognized_ticket_exception_when_fetch_car_given_a_fake_ticket_and_parking_boy_with_two_empty_parking_lots() {
         // GIVEN
-        ParkingLot parkingLot1 = new ParkingLot(10);
-        ParkingLot parkingLot2 = new ParkingLot(10);
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(parkingLot1);
-        parkingLots.add(parkingLot2);
+        Ticket fakeTicket = new Ticket();
+
+        ParkingLot parkingLot1 = Mockito.mock(ParkingLot.class);
+        ParkingLot parkingLot2 = Mockito.mock(ParkingLot.class);
+        List<ParkingLot> parkingLots = new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2));
+        when(parkingLot1.isInParkingLot(fakeTicket)).thenReturn(false);
+        when(parkingLot2.isInParkingLot(fakeTicket)).thenReturn(false);
 
         StandardParkingBoy standardParkingBoy = new StandardParkingBoy(parkingLots);
-
-        Ticket fakeTicket = new Ticket();
 
         // THEN
         assertThrows(
